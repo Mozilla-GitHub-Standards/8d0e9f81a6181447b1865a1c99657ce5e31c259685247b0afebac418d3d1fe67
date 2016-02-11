ac55c00ddc65d76ed58736e5cd48e4da00709e45
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2016-02-10 18:16:05 -0800

    Corrected R to Rscript

--- rpackageinstalls.sh
+++ rpackageinstalls.sh
@@ -1,5 +1,7 @@
-## Expects R to be present ( 3.2 +)
+## Expects R to be present ( 3.2 +), running this 
+## Rscript -e 'cat(sprintf("%s.%s\\n",R.version["major"],R.version["minor"]))'
+## will return the R version
 ## This line will install the following packages
 
-sudo R -e 'for(x in c("httr","infuser","rjson","data.table","arm","forecast","zoo","DescTools","mixtools")) install.packages(x,repo="https://cran.cnr.berkeley.edu/",dep=TRUE)'
+sudo Rscript -e 'for(x in c("httr","infuser","rjson","data.table","arm","forecast","zoo","DescTools","mixtools")) install.packages(x,repo="https://cran.cnr.berkeley.edu/",dep=TRUE)'
 
